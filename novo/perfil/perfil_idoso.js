2618079d49a0bd4ea6f5594b2af5323273741268
joycecatarine <joycecatarine@yahoo.com>
2023-11-28 22:28:41 -0300

    Atualização

---
+++ novo/perfil/perfil_idoso.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function carregarDadosAcompanhante() {
+        const url = 'http://localhost:8080/mostrarIdosos/1';
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                exibirDadosAcompanhante(data);
+            })
+            .catch(error => {
+                console.error('Erro na requisição:', error);
+            });
+    }
+
+    function exibirDadosAcompanhante(acompanhante) {
+        document.getElementById('usuariodois').innerText = acompanhante.usuariodois;
+		document.getElementById('usuario').innerText = acompanhante.usuario;
+		document.getElementById('nome').innerText = acompanhante.nome;
+        document.getElementById('email').innerText = acompanhante.email;
+		document.getElementById('genero').innerText = acompanhante.genero;
+		document.getElementById('endereco').innerText = acompanhante.endereco;
+        document.getElementById('suporte').innerText = acompanhante.suporte;
+        document.getElementById('telefone').innerText = acompanhante.telefone;
+        document.getElementById('nascimento').innerText = acompanhante.nascimento;
+        document.getElementById('hobbie').innerText = acompanhante.hobbie;
+    }
+
+    carregarDadosAcompanhante();
+});
